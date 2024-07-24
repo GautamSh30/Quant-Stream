0ef9412b6f47ea0ad60edfe15f826c87904c96e6
Gautam Sharma <gautam2586272@gmail.com>
2024-07-24 11:20:19 +0530

    auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
-  return <Button size="lg">Click me</Button>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <h1>DashBoard</h1>
+      <UserButton />
+    </div>
+  );
 }
